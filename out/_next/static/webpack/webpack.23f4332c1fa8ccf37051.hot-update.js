b1234bdabd335ed6d16e37182b3ea2c4b2c8c948
Werner Ovalle <32042983+WernerOvalle@users.noreply.github.com>
2022-07-23 17:43:28 +0000

    prueba vercel

--- out/_next/static/webpack/webpack.23f4332c1fa8ccf37051.hot-update.js
+++ out/_next/static/webpack/webpack.23f4332c1fa8ccf37051.hot-update.js
@@ -12,19 +12,8 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ 
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "23f4332c1fa8ccf37051"; }
+/******/ 	__webpack_require__.h = function() { return "474110ac9200094e60db"; }
 /******/ }();
 /******/ 
-/******/ /* webpack/runtime/compat */
-/******/ 
-/******/ 
-/******/ // noop fns to prevent runtime errors during initialization
-/******/ if (typeof self !== "undefined") {
-/******/ 	self.$RefreshReg$ = function () {};
-/******/ 	self.$RefreshSig$ = function () {
-/******/ 		return function (type) {
-/******/ 			return type;
-/******/ 		};
-/******/ 	};
-/******/ }}
+/******/ }
 );

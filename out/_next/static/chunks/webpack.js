5fa8601d9aee2392939bbe34f1dd2e283f40a2b6
Werner Ovalle <32042983+WernerOvalle@users.noreply.github.com>
2022-08-04 17:49:01 +0000

    optimize images

--- out/_next/static/chunks/webpack.js
+++ out/_next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "00b79bfdb22d15a06243"; }
+/******/ 		__webpack_require__.h = function() { return "48e84cbd8345f78d49f1"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
